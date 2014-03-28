3194d917aef914e08b064ac004da10ffcb7cb054
Prakhar Srivastav <prakhar1989@gmail.com>
2014-03-29 00:18:54 +0530

    Got rid of request marshallers

--- src/Newman.js
+++ src/Newman.js
@@ -1,6 +1,6 @@
 var jsface = require("jsface"),
 	CollectionRunner = require("./runners/CollectionRunner"),
-	RequestMarshaller = require("./marshallers/RequestMarshaller");
+	CollectionModel = require('./models/CollectionModel.js');
 
 /**
  * @class Newman
@@ -10,7 +10,7 @@ var Newman = jsface.Class({
 	$singleton: true,
 
 	execute: function(requestJSON) {
-		var marshalledCollection = new RequestMarshaller(requestJSON).getMarshalledCollection();
+		var marshalledCollection = new CollectionModel(requestJSON).getOrderedRequests();
 
 		var runner = new CollectionRunner(marshalledCollection);
 		runner.execute();
