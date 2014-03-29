3dc0fd94d605323cac778bedbdfa9a69731baf07
Prakhar Srivastav <prakhar1989@gmail.com>
2014-03-29 12:36:45 +0530

    Missing documentation

--- src/runners/AbstractRunner.js
+++ src/runners/AbstractRunner.js
@@ -1,5 +1,9 @@
 var jsface = require("jsface");
 
+/**
+ * @class AbstractRunner
+ * @param CollectionJson {JSON} Takes a JSON as the input
+ */
 var AbstractRunner = jsface.Class({
 	constructor: function(collection) {
 		this.collection = collection || [];
