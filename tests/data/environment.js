ab5b26c06919aa69fce0209232811d10d322a34f
Prakhar Srivastav <prakhar1989@gmail.com>
2014-04-01 00:43:10 +0300

    Added a sample environment js for tests

---
+++ tests/data/environment.js
@@ -0,0 +1,21 @@
+{
+    "id": "4fc5e504-f8c1-07fb-e877-1850a7218148",
+    "name": "postman-demo",
+    "values": [
+        {
+            "key": "url",
+            "value": "http://dump.getpostman.com/",
+            "type": "text",
+            "name": "url"
+        },
+        {
+            "key": "user_id",
+            "value": "1",
+            "type": "text",
+            "name": "user_id"
+        }
+    ],
+    "timestamp": 1396006402112,
+    "synced": false,
+    "syncedFilename": ""
+}
