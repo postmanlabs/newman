26fa1d8b45dc1730754028775e6dbf27e77fab99
Prakhar Srivastav <prakhar1989@gmail.com>
2014-04-06 14:55:11 +0300

    moved colors into response handler"

--- src/responseHandlers/DefaultResponseHandler.js
+++ src/responseHandlers/DefaultResponseHandler.js
@@ -23,9 +23,17 @@ var DefaultResponseHandler = jsface.Class([EventEmitter], {
 	// function called when the event "requestExecuted" is fired. Takes 4 self-explanatory paramters
 	_onRequestExecuted: function(error, response, body, request) {
 		if (error) {
-			log.error(request.id + " terminated with the error " + error.code);
+			log.error(request.id + " terminated with the error " + error.code + "\n");
 		} else {
-			log.success(request.url + " succeded with response.");
+			// TODO, @prakhar1989 can you please refactor this into your Response handler.
+			if (response.statusCode >= 200 && response.statusCode < 300) {
+				log.success(response.statusCode);
+			} else {
+				log.error(response.statusCode);
+			}
+			log.notice(" " + response.stats.timeTaken + "ms");
+			log.normal(" " + request.name);
+			log.light(" " + request.description + "\n");
 		}
 	}
 });
