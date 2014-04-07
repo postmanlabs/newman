9f311a6e75d14b35e7d11c6b541fce4aacc7382b
Prakhar Srivastav <prakhar1989@gmail.com>
2014-04-07 21:48:53 +0300

    Initial set of testcases and logging

--- src/responseHandlers/TestResponseHandler.js
+++ src/responseHandlers/TestResponseHandler.js
@@ -1,5 +1,6 @@
 var jsface = require('jsface'),
-	log    = require('../utilities/Logger');
+	log    = require('../utilities/Logger'),
+	_und   = require('underscore');
 
 /**
  * @class TestResponseHandler
@@ -11,12 +12,56 @@ var TestResponseHandler = jsface.Class({
 	execute: function(request, response) {
 		this.request = request;
 		this.response = response;
+		this.tests = this.getTestResults();
+		this._logTestResults(this.tests);
 	},
 
-	_hasTestcases: function() {
+	_hasTestCases: function() {
 		return (this.request.tests !== undefined);
-	}
+	},
 
+	_getValidTestCases: function() {
+		var testCases = this.request.tests.split(';');
+		var tests = [];
+		_und.each(testCases, function(testCase) {
+			var t = testCase.trim();
+			if (t.length > 0) {
+				tests.push(t);
+			}
+		});
+		return tests;
+	},
+
+	getTestResults: function() {
+		var tests = {}
+		var testCases = this._getValidTestCases();
+		_und.each(testCases, function(testCase) {
+			_und.extend(tests, this._evalutate(testCase));
+		}, this);
+		return tests;
+	},
+
+	_evalutate: function(testCase) {
+		// sets the environment
+		var tests = {};
+		var responseCode = this.response.statusCode;
+		var responseHeaders = this.response.headers;
+		var responseTime = parseInt(this.response.stats.timeTaken);
+		
+		// call eval
+		eval(testCase);
+		return tests;
+	},
+
+	_logTestResults: function(tests) {
+		_und.each(_und.keys(tests), function(key) {
+			if (tests[key]) {
+				log.success(key);
+			} else {
+				log.error(key);
+			}
+		});
+	}
 });
 
 module.exports = TestResponseHandler;
--- tests/TestResponseHandler.js
+++ tests/TestResponseHandler.js
@@ -13,10 +13,23 @@ describe("ResponseHandlerFactory", function() {
 		var filePath = path.join(__dirname, 'data', 'PostmanCollection.json');
 		var url = "https://www.getpostman.com/collections/fc3f0598daaa5271e4f7";
 		this.collectionJson = JSON5.parse(fs.readFileSync(filePath, 'utf8'));
-		this.request = this.collection.requests[0];
+		this.request = this.collectionJson.requests[0];
+		this.response = {
+			headers: { 
+				'server': 'nginx/1.1.19',
+				'content-type': 'application/json',
+				'content-length': '3',
+				'connection': 'Keep-Alive',
+				'age': '286' 
+			},
+			stats: {timeTaken: "100"},
+			statusCode: 200,
+		};
 	});
 	
-	it("test", function() {
-		console.log(this.collectionJson.requests[0]);
+	it("should return false for request for no test cases", function() {
+		this.request.tests = 'tests["statuscode is 200"] = responseCode === 200;' + 
+							 '\n\ntests["response has content-type"] = responseHeaders["content-type"] == "application/json";';
+		TestResponseHandler.execute(this.request, this.response);
 	});
 });
