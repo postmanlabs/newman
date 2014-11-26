e4f81c7a0121d788e9b45709196a359e2c5713e7
dog94 <dog94@163.com>
2014-11-26 15:29:03 -0800

    documentation on HttpStatusCodes.js

--- src/utilities/HttpStatusCodes.js
+++ src/utilities/HttpStatusCodes.js
@@ -1,3 +1,10 @@
+/*
+ * Interprets HTTP status according to their status codes
+ * @return the details of each HTTP status
+ */
+
+
+
 var jsface  = require("jsface");
 
 var HttpStatusCodes = jsface.Class({
