a3c90b7cc65bb11b04198e48a430ddb250afbbee
yoonjib <yoonji_519@hotmail.com>
2014-11-27 13:49:57 -0800

    Added total passes andtotal failures

--- src/runners/IterationRunner.js
+++ src/runners/IterationRunner.js
@@ -179,7 +179,7 @@ var IterationRunner = jsface.Class([Options, EventEmitter], {
 
     // logs the iteration count
     _logStatus: function() {
-        log.note("\nIterationNumber " + this.iteration + " of " + this.numOfIterations + "\n");
+        log.note("IterationNumber " + this.iteration + " of " + this.numOfIterations + "\n");
     },
 
     // set the global envjson and then run the next iteration
@@ -192,7 +192,9 @@ var IterationRunner = jsface.Class([Options, EventEmitter], {
             this._runCollection();
             Globals.envJson = currentGlobalEnv;
         } else {
-            log.note("\nIn total " + Globals.fail + " of " + Globals.totalTests + " tests failed\n");
+            var totalPass=Globals.totalTests-Globals.fail;
+            log.warn("\nTotal Passes: "+totalPass+ "/"+Globals.totalTests+ "\n");
+            log.warn("Total Failures: " + Globals.fail + "/" +Globals.totalTests +"\n");
             this._exportResponses();
             // connie - runs line of log if collection is successful
             //this._addEventListeners();
