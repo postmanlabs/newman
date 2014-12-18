705661160df27737f6c15c3a81250385b87007d5
Abhijit Kane <abhijitkane@gmail.com>
2014-12-18 12:01:34 +0530

    Version bump to 1.2.3

--- src/utilities/Globals.js
+++ src/utilities/Globals.js
@@ -8,7 +8,7 @@ var jsface = require('jsface');
 var Globals = jsface.Class({
 	$singleton: true,
 
-	newmanVersion: "1.2.2",
+	newmanVersion: "1.2.3",
 
 	/**
 	 * Used to add the Globals used through out the app
