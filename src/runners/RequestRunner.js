fba0b1eb6a60c0b9aa5950b3a29b58cb6c757d5b
Vincent Boutour <vboutour@omnikles.com>
2015-02-20 14:47:45 +0100

    Adding helper use in case of unrefreshed Authorization header

--- src/runners/RequestRunner.js
+++ src/runners/RequestRunner.js
@@ -217,6 +217,13 @@ var RequestRunner = jsface.Class([Queue, EventEmitter], {
         RequestOptions.followAllRedirects = true;
         RequestOptions.jar = true;
         this._setBodyData(RequestOptions, request);
+
+        if (request.helpers && request.helpers.id) {
+            RequestOptions.auth = {
+                user: request.transformed.helpers.username,
+                pass: request.transformed.helpers.password
+            };
+        }
         return RequestOptions;
     },
 
