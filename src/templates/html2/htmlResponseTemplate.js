31bc12471f0df3b91ad413f72c9d3ca547fe983b
Maelongalad <maelongalad@gmail.com>
2015-04-07 15:05:39 +0300

    fix relative paths
    rm debug line

--- src/templates/html2/htmlResponseTemplate.js
+++ src/templates/html2/htmlResponseTemplate.js
@@ -1,19 +1,19 @@
-var util = require('util');
+var path = require('path');
 var _ = require('lodash');
 var fs = require('fs');
 
 function template (obj) {
-    var tpl = fs.readFileSync('./src/templates/html2/template.html', 'utf-8');
+    var tpl = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf-8');
     var data = {
         data: obj,
         materializeCss: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.0/css/materialize.min.css',
         materializeJs: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.0/js/materialize.min.js',
         jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',
-        customCss: fs.readFileSync('./src/templates/html2/style.css', 'utf-8'),
-        customJs: fs.readFileSync('./src/templates/html2/script.js', 'utf-8'),
-        prismCss: fs.readFileSync('./src/templates/html2/prism.css', 'utf-8'),
-        prismJs: fs.readFileSync('./src/templates/html2/prism.js', 'utf-8'),
-        markdownJs: fs.readFileSync('./src/templates/html2/markdown.min.js', 'utf-8')
+        customCss: fs.readFileSync(path.join(__dirname, 'style.css'), 'utf-8'),
+        customJs: fs.readFileSync(path.join(__dirname, 'script.js'), 'utf-8'),
+        prismCss: fs.readFileSync(path.join(__dirname, 'prism.css'), 'utf-8'),
+        prismJs: fs.readFileSync(path.join(__dirname, 'prism.js'), 'utf-8'),
+        markdownJs: fs.readFileSync(path.join(__dirname, 'markdown.min.js'), 'utf-8')
     };
     var compiled = _.template(tpl);
 
