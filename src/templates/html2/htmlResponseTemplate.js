f070fca8a3bfce3cfb43c8fd696fa7f1e5aac190
Maelongalad <maelongalad@gmail.com>
2015-04-08 12:10:05 +0300

    renamed .html -> .ejs for syntax highlighting; formatted and renamed variables into more explaining; urlencoded form output

--- src/templates/html2/htmlResponseTemplate.js
+++ src/templates/html2/htmlResponseTemplate.js
@@ -3,21 +3,32 @@ var _ = require('lodash');
 var fs = require('fs');
 
 function template (obj) {
-    var tpl = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf-8');
-    var data = {
-        data: obj,
+    var template = read('template.ejs');
+    var fragments = {
+        header: _.template(read('fragments/navigation.ejs'))(obj),
+        mainPage: _.template(read('fragments/main_page.ejs'))(obj),
+        requestPage: _.template(read('fragments/request_page.ejs'))(obj)
+    };
+    var statics = {
+        customCss: read('statics/style.css'),
+        customJs: read('statics/script.js'),
+        prismCss: read('statics/prism.css'),
+        prismJs: read('statics/prism.js'),
+        markdownJs: read('statics/markdown.min.js'),
         materializeCss: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.0/css/materialize.min.css',
         materializeJs: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.0/js/materialize.min.js',
-        jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',
-        customCss: fs.readFileSync(path.join(__dirname, 'style.css'), 'utf-8'),
-        customJs: fs.readFileSync(path.join(__dirname, 'script.js'), 'utf-8'),
-        prismCss: fs.readFileSync(path.join(__dirname, 'prism.css'), 'utf-8'),
-        prismJs: fs.readFileSync(path.join(__dirname, 'prism.js'), 'utf-8'),
-        markdownJs: fs.readFileSync(path.join(__dirname, 'markdown.min.js'), 'utf-8')
+        jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'
     };
-    var compiled = _.template(tpl);
+    var compiled = _.template(template);
 
-    return compiled(data);
+    return compiled({
+        fragments: fragments,
+        statics: statics
+    });
+}
+
+function read (name) {
+    return fs.readFileSync(path.join(__dirname, name), 'utf-8');
 }
 
 module.exports = template;
