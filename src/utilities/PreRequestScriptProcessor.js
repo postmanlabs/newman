1b9c0db268413ac182e42ed90e3ea40ca790a5c1
Kevin Ennis <kennis84@gmail.com>
2015-09-09 08:12:51 -0400

    Don't access 'arguments' or 'caller' properties on Function.prototype
    ES6 has deprecated these properties, and replaced them with accessors that just throw a TypeError if you try to touch them.
    
    My workaround is to simply copy *entire file descriptors* instead of direct assignment. This way, you maintain enumerability, setters/getters, etc.

--- src/utilities/PreRequestScriptProcessor.js
+++ src/utilities/PreRequestScriptProcessor.js
@@ -62,7 +62,7 @@ var PreRequestScriptProcessor = jsface.Class({
         sweet += "for(p in sugar.array)  {if(p==='create'){Array.create=sugar.array.create} else{Array.prototype[p]= sugar.array[p];}}";
         sweet += "for(p in sugar.string) String.prototype[p]  = sugar.string[p];";
         sweet += "for(p in sugar.date)  {if(p==='create'){Date.create=sugar.date.create} else{Date.prototype[p]= sugar.date[p];}}";
-        sweet += "for(p in sugar.funcs)  Function.prototype[p]= sugar.funcs[p];";
+        sweet += "for(p in sugar.funcs) Object.defineProperty(Function.prototype, p, Object.getOwnPropertyDescriptor(sugar.funcs, p));";
 
         //to ensure that environment. and global. references are updated
         var setEnvHack = "postman.setEnvironmentVariable = function(key,val) {postman.setEnvironmentVariableReal(key,val);environment[key]=val;};";
@@ -138,7 +138,9 @@ var PreRequestScriptProcessor = jsface.Class({
             sugar.date[p] = Date.prototype[p];
         });
         sugar.date["create"] = Date.create;
-        //Object.getOwnPropertyNames(Function.prototype).each(function(p) { sugar.funcs[p] = Function.prototype[p];});
+        Object.getOwnPropertyNames(Function.prototype).each(function(p) {
+            Object.defineProperty(sugar.funcs, p, Object.getOwnPropertyDescriptor(Function.prototype, p));
+        });
         return {
             sugar: sugar,
             request: {
