de2449ada23d5690160988045ab2746d5dacc206
Abhijit Kane <abhijitkane@gmail.com>
2015-09-20 22:25:58 +0530

    Adding -R option to avoid redirects

--- src/runners/RequestRunner.js
+++ src/runners/RequestRunner.js
@@ -230,8 +230,7 @@ var RequestRunner = jsface.Class([Queue, EventEmitter], {
 		RequestOptions.method = request.method;
 		RequestOptions.headers = Helpers.generateHeaderObj(request.transformed.headers);
 		RequestOptions.followAllRedirects = false;
-        //TODO: Add an option for this
-        //RequestOptions.followRedirect = false;
+        RequestOptions.followRedirect = !Globals.avoidRedirects;
 		RequestOptions.jar = true;
 		RequestOptions.timeout = this.requestTimeout;
         if(Globals.responseEncoding) {
