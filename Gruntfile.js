7b614e52671f18ff59edc6dd775094325cffa821
Aniket Panse <aniket@getpostman.com>
2015-10-01 20:33:53 +0530

    fixed #211, and added a test for it

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,8 @@ module.exports = function(grunt) {
 					'bin/newman -c tests/integ_tests/crypto-md5.json.postman_collection -s && ' +
 					'bin/newman -c tests/integ_tests/esc.postman_collection -e tests/integ_tests/esc.postman_environment -s && ' +
 					'bin/newman -c tests/integ_tests/prototypeCheck.json.postman_collection -s && ' +
-					'bin/newman -c tests/integ_tests/redirectTest.json.postman_collection -s -R'
+					'bin/newman -c tests/integ_tests/redirectTest.json.postman_collection -s -R && ' +
+					'bin/newman -c tests/integ_tests/multipleFormValues.json.postman_collection -s'
     		}
 		}
 	});
