c3912377f62e3d258543e2d88217fa484b25950b
Aniket Panse <aniket@getpostman.com>
2015-10-01 20:13:32 +0530

    reformatted abstractresponsehandler, fixed verbose output bug

--- src/responseHandlers/AbstractResponseHandler.js
+++ src/responseHandlers/AbstractResponseHandler.js
@@ -1,11 +1,12 @@
-var jsface           = require('jsface'),
-	log              = require('../utilities/Logger'),
-	Globals           = require('../utilities/Globals'),
-	path              = require('path'),
-	fs                = require('fs'),
-	ErrorHandler     = require('../utilities/ErrorHandler'),
-	EventEmitter     = require('../utilities/EventEmitter'),
-	ResponseExporter = require('../utilities/ResponseExporter');
+var jsface = require('jsface'),
+    log = require('../utilities/Logger'),
+    Globals = require('../utilities/Globals'),
+    path = require('path'),
+    fs = require('fs'),
+    _ = require('lodash'),
+    ErrorHandler = require('../utilities/ErrorHandler'),
+    EventEmitter = require('../utilities/EventEmitter'),
+    ResponseExporter = require('../utilities/ResponseExporter');
 
 /**
  * @class AbstractResponseHandler
@@ -13,66 +14,75 @@ var jsface           = require('jsface'),
  * @mixes EventEmitter
  */
 var AbstractResponseHandler = jsface.Class([EventEmitter], {
-	$singleton: true,
+    $singleton: true,
 
-	/**
-	 * Sets up the event listener for the request executed event emitted on each
-	 * request execution
-	 * @memberOf AbstractResponseHandler
-	 */
-	initialize: function() {
-		this._bindedOnRequestExecuted = this._onRequestExecuted.bind(this);
-		this.addEventListener('requestExecuted', this._bindedOnRequestExecuted);
-	},
+    /**
+     * Sets up the event listener for the request executed event emitted on each
+     * request execution
+     * @memberOf AbstractResponseHandler
+     */
+    initialize: function () {
+        this._bindedOnRequestExecuted = this._onRequestExecuted.bind(this);
+        this.addEventListener('requestExecuted', this._bindedOnRequestExecuted);
+    },
 
-	// method to be over-ridden by the inheriting classes
-	_onRequestExecuted: function(error, response, body, request, tests) {
-		if (error) {
-			ErrorHandler.requestError(request, error);
-		} else  {
-			this._printResponse(error, response, body, request);
-		}
-		ResponseExporter.addResult(request, response, tests);
-	},
+    // method to be over-ridden by the inheriting classes
+    _onRequestExecuted: function (error, response, body, request, tests) {
+        if (error) {
+            ErrorHandler.requestError(request, error);
+        } else {
+            this._printResponse(error, response, body, request);
+        }
+        ResponseExporter.addResult(request, response, tests);
+    },
 
-	_printResponse: function(error, response, body, request) {
+    _printResponse: function (error, response, body, request) {
         if (Globals.outputFileVerbose) {
             var filepath = path.resolve(Globals.outputFileVerbose);
+            var requestData;
+
+            try {
+                requestData = JSON.stringify(_.object(_.pluck(request.transformed.data, "key"),
+                    _.pluck(request.transformed.data, "value")), null, 2);
+            }
+            catch (e) {
+                // Not JSON.
+                requestData = request.transformed.data;
+            }
 
             var requestString = "-------------------------------------------------------------------------------------------\n" +
-                    response.statusCode + " " +
-                    response.stats.timeTaken + "ms" + " " +
-                    request.name + " " +  "[" + request.method + "] " +
-                    request.transformed.url +
-                    "\n------------------------------------------------------------" +
-                    "\nRequest headers:\n" +
-                    JSON.stringify(response.req._headers, undefined, 1) +
-                    "\nRequest body:\n" +
-                    request.transformed.data +
-                    "\n------------------------------------------------------------" +
-                    "\nResponse headers:\n" +
-                    JSON.stringify(response.headers, undefined, 1) +
-                    "\nResponse body:\n" + response.body + "\n";
+                response.statusCode + " " +
+                response.stats.timeTaken + "ms" + " " +
+                request.name + " " + "[" + request.method + "] " +
+                request.transformed.url +
+                "\n------------------------------------------------------------" +
+                "\nRequest headers:\n" +
+                JSON.stringify(response.req._headers, undefined, 1) +
+                "\nRequest data:\n" +
+                requestData +
+                "\n------------------------------------------------------------" +
+                "\nResponse headers:\n" +
+                JSON.stringify(response.headers, undefined, 1) +
+                "\nResponse body:\n" + response.body + "\n";
 
-            fs.appendFileSync(filepath , requestString);
+            fs.appendFileSync(filepath, requestString);
         }
 
-		if (response.statusCode >= 200 && response.statusCode < 300) {
-			log.success(response.statusCode);
-		} else {
-			ErrorHandler.responseError(response);
-		}
-		log
-		.notice(" " + response.stats.timeTaken + "ms")
-		.normal(" " + request.name + " ")
-		.notice("[" + request.method + "] ")
-		.light( request.transformed.url + "\n");
-	},
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+            log.success(response.statusCode);
+        } else {
+            ErrorHandler.responseError(response);
+        }
+        log.notice(" " + response.stats.timeTaken + "ms")
+            .normal(" " + request.name + " ")
+            .notice("[" + request.method + "] ")
+            .light(request.transformed.url + "\n");
+    },
 
-	// clears up the set event
-	clear: function() {
-		this.removeEventListener('requestExecuted', this._bindedOnRequestExecuted);
-	}
+    // clears up the set event
+    clear: function () {
+        this.removeEventListener('requestExecuted', this._bindedOnRequestExecuted);
+    }
 });
 
 module.exports = AbstractResponseHandler;
