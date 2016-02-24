1088526ba2970eabf0b3cb1bad6f832395a786b4
Aniket Panse <aniket@getpostman.com>
2016-02-24 19:05:01 +0530

    updated deprecation notice for v1.x

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -5,5 +5,5 @@ console.log('*  If you are using Node v4.0+, you should install Newman v2.x:  *'
 console.log('*                                                                *');
 console.log('*                   npm install newman@2                         *');
 console.log('*                                                                *');
-console.log('*  Deprecation: Newman v1.x will be deprecated soon.             *');
+console.log('*  Deprecation: Newman v1.x has been deprecated.                 *');
 console.log('******************************************************************');
