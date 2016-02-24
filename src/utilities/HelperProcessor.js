04570ba1616e8b648c7f0cd688e41e29072faa7d
Harshavardhana <harsha@harshavardhana.net>
2016-02-24 14:09:33 -0800

    awsSignv4: Collection properties should be 'service' not 'serviceName'
    Collection properties for helper attributes lists 'service' not 'serviceName'.
    ```
    "helperAttributes": {
    	"id": "awsSigV4",
    ...
    ...
    	"service": "s3",
    	"auto": true,
    	"saveHelper": true
    },
    ```
    
    This fixes a bug where 'execute-api' is used as part of Signature calculation
    and failing.

--- src/utilities/HelperProcessor.js
+++ src/utilities/HelperProcessor.js
@@ -57,7 +57,7 @@ var HelperProcessor = jsface.Class({
         var signedParams = aws4.sign({
             host: parsedURL.hostname,
             path: parsedURL.path,
-            service: properties.serviceName || 'execute-api',
+            service: properties.service || 'execute-api',
             region: properties.region,
             method: request.method,
             body: body,
