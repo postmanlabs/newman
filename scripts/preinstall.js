3ba4aaa50f4eeb6f5bc1c2f546a5978d5bdbcb1c
Aniket Panse <aniket@getpostman.com>
2016-02-24 13:20:34 +0530

    updated 1.x notice

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,9 +1,9 @@
-console.log('****************************************************************');
-console.log('*                          Notice                              *');
-console.log('****************************************************************');
-console.log('* If you are using Node v4.0+, you should install Newman v2.x: *');
-console.log('*                                                              *');
-console.log('*                  npm install newman@2                        *');
-console.log('*                                                              *');
-console.log('* Note that Newman v1.x will be deprecated soon.               *');
-console.log('****************************************************************');
+console.log('******************************************************************');
+console.log('*                           Notice                               *');
+console.log('******************************************************************');
+console.log('*  If you are using Node v4.0+, you should install Newman v2.x:  *');
+console.log('*                                                                *');
+console.log('*                   npm install newman@2                         *');
+console.log('*                                                                *');
+console.log('*  Deprecation: Newman v1.x will be deprecated soon.             *');
+console.log('******************************************************************');
