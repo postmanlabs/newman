95177f89bb9c3ddd7e6a6a2089b643b860525028
Shamasis Bhattacharya <mail@shamasis.net>
2016-07-13 20:31:06 +0530

    Sanity unit tests around failing cases of newman.run

--- test/unit/run.test.js
+++ test/unit/run.test.js
@@ -1,5 +1,6 @@
 var expect = require('expect.js'),
-    sdk = require('postman-collection');
+    sdk = require('postman-collection'),
+    async = require('async');
 
 /* global describe, it */
 describe('run module', function () {
@@ -17,9 +18,56 @@ describe('run module', function () {
         }).not.throwException();
     });
 
-    it('must start a run with empty collection object', function (done) {
+    it('must error out if collection is absent in options', function (done) {
+        expect(run).withArgs({}, function (err) {
+            expect(err).be.ok();
+            expect(err && err.message).be('newman: expecting a collection to run');
+
+            done();
+        }).not.throwException();
+    });
+
+    it('must start a run with empty collection as plain object', function (done) {
+        expect(run).withArgs({
+            collection: {}
+        }, done).not.throwException();
+    });
+
+    it('must start a run with empty collection as SDK instance', function (done) {
         expect(run).withArgs({
             collection: new sdk.Collection()
         }, done).not.throwException();
+    });
+
+    it('must gracefully send error to callback on garbage collection', function (done) {
+        async.parallel([
+            function (next) {
+                expect(run).withArgs({
+                    collection: 'garbage'
+                }, function (err) {
+                    expect(err).be.ok();
+                    expect(err && err.message).be('newman: expecting a collection to run');
+                    next();
+                }).not.throwException();
+            },
+            function (next) {
+                expect(run).withArgs({
+                    collection: 3.14
+                }, function (err) {
+                    expect(err).be.ok();
+                    expect(err && err.message).be('newman: expecting a collection to run');
+                    next();
+                }).not.throwException();
+            },
+            function (next) {
+                expect(run).withArgs({
+                    collection: null
+                }, function (err) {
+                    expect(err).be.ok();
+                    expect(err && err.message).be('newman: expecting a collection to run');
+                    next();
+                }).not.throwException();
+            }
+        ], done);
     });
 });
