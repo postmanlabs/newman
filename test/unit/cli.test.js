d1e3c4796c2b17f1c2a3f71ad2200ac6700d54be
Aniket Panse <aniket@getpostman.com>
2016-07-26 16:43:17 +0530

    updated the tests to use the new option

--- test/unit/cli.test.js
+++ test/unit/cli.test.js
@@ -56,7 +56,7 @@ describe('cli parser', function () {
                 expect(opts.globals).to.be('myGlobals.json');
                 expect(opts.exportGlobals).to.be('exported_glob.json');
                 expect(opts.delay).to.be(12000);
-                expect(opts.requestTimeout).to.be(5000);
+                expect(opts.timeoutRequest).to.be(5000);
                 expect(opts.avoidRedirects).to.be(true);
                 expect(opts.insecure).to.be(true);
                 expect(opts.tls).to.be(true);
@@ -141,8 +141,8 @@ describe('cli parser', function () {
                 expect(opts.globals).to.be('myGlobals.json');
                 expect(opts.exportGlobals).to.be('exported_glob.json');
                 expect(opts.delay).to.be(12000);
-                expect(opts.requestTimeout).to.be(5000);
-                expect(opts.avoidRedirects).to.be(true);
+                expect(opts.timeoutRequest).to.be(5000);
+                expect(opts.ignoreRedirects).to.be(true);
                 expect(opts.insecure).to.be(true);
                 expect(opts.tls).to.be(true);
                 expect(opts.encoding).to.be('binary');
@@ -193,8 +193,8 @@ describe('cli parser', function () {
                 expect(opts.globals).to.be('myGlobals.json');
                 expect(opts.exportGlobals).to.be('exported_glob.json');
                 expect(opts.delay).to.be(12000);
-                expect(opts.requestTimeout).to.be(5000);
-                expect(opts.avoidRedirects).to.be(true);
+                expect(opts.timeoutRequest).to.be(5000);
+                expect(opts.ignoreRedirects).to.be(true);
                 expect(opts.insecure).to.be(true);
                 expect(opts.tls).to.be(true);
                 expect(opts.encoding).to.be('binary');
