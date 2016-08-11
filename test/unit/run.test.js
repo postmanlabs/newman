ec3148086b0a39bda849fd31dc40e153e61138bc
Shamasis Bhattacharya <mail@shamasis.net>
2016-08-12 04:52:18 +0530

    Removed example collection that was used for test but was not localised Also removed unit tests that had no side-effect

--- test/unit/run.test.js
+++ test/unit/run.test.js
@@ -1,11 +1,10 @@
-var path = require('path'),
-    async = require('async'),
-    expect = require('expect.js'),
-    sdk = require('postman-collection');
+var expect = require('expect.js'),
+    sdk = require('postman-collection'),
+    async = require('async');
 
 /* global describe, it */
 describe('run module', function () {
-    var run = require(path.join(__dirname, '..', '..', 'lib', 'run'));
+    var run = require('../../lib/run');
 
     it('must export a function', function () {
         expect(run).be.a('function');
@@ -66,116 +65,6 @@ describe('run module', function () {
                 }, function (err) {
                     expect(err).be.ok();
                     expect(err && err.help).be('unable to read data from file "abcd"');
-                    next();
-                }).not.throwException();
-            }
-        ], done);
-    });
-
-    it('exits with code 0 for all 4 --bail, -x combinations (angel-collection)', function (done) {
-        var sampleCollection = path.join(__dirname, '..', '..', 'examples', 'angel.postman_collection.json');
-
-        async.parallel([
-            // run with neither bail nor suppressExitCode
-            function (next) {
-                expect(run).withArgs({
-                    collection: sampleCollection
-                }, function (err, summary) {
-                    expect(err).be(null);
-                    expect(summary.run.error).be(null);
-                    expect(summary.run.failures.length).to.be(0);
-                    next();
-                }).not.throwException();
-            },
-            // run with bail, but not suppressExitCode
-            function (next) {
-                expect(run).withArgs({
-                    collection: sampleCollection,
-                    bail: true
-                }, function (err, summary) {
-                    expect(err).be(null);
-                    expect(summary.run.error).be(null);
-                    expect(summary.run.failures.length).to.be(0);
-                    next();
-                }).not.throwException();
-            },
-            // run with suppressExitCode, but not bail
-            function (next) {
-                expect(run).withArgs({
-                    collection: sampleCollection,
-                    suppressExitCode: true
-                }, function (err, summary) {
-                    expect(err).be(null);
-                    expect(summary.run.error).be(null);
-                    expect(summary.run.failures.length).to.be(0);
-                    next();
-                }).not.throwException();
-            },
-            // run with both bail and suppressExitCode
-            function (next) {
-                expect(run).withArgs({
-                    collection: sampleCollection,
-                    bail: true,
-                    suppressExitCode: true
-                }, function (err, summary) {
-                    expect(err).be(null);
-                    expect(summary.run.error).be(null);
-                    expect(summary.run.failures.length).to.be(0);
-                    next();
-                }).not.throwException();
-            }
-        ], done);
-    });
-
-    it('exits with a correct exit code for all 4 --bail, -x combinations (devil-collection)', function (done) {
-        var badCollection = path.join(__dirname, '..', '..', 'examples', 'devil.postman_collection.json');
-
-        async.parallel([
-            // run with neither bail nor suppressExitCode
-            function (next) {
-                expect(run).withArgs({
-                    collection: badCollection
-                }, function (err, summary) {
-                    expect(err).not.be.ok();
-                    expect(summary.run.error).be(null);
-                    expect(summary.run.failures.length).to.be(1);
-                    next();
-                }).not.throwException();
-            },
-            // run with bail, but not suppressExitCode
-            function (next) {
-                expect(run).withArgs({
-                    collection: badCollection,
-                    bail: true
-                }, function (err, summary) {
-                    expect(err).not.be.ok();
-                    expect(summary.run.error).be(null);
-                    expect(summary.run.failures.length).to.be(2);
-                    next();
-                }).not.throwException();
-            },
-            // run with suppressExitCode, but not bail
-            function (next) {
-                expect(run).withArgs({
-                    collection: badCollection,
-                    suppressExitCode: true
-                }, function (err, summary) {
-                    expect(err).be(null);
-                    expect(summary.run.error).be(null);
-                    expect(summary.run.failures.length).to.be(1);
-                    next();
-                }).not.throwException();
-            },
-            // run with both bail and suppressExitCode
-            function (next) {
-                expect(run).withArgs({
-                    collection: badCollection,
-                    bail: true,
-                    suppressExitCode: true
-                }, function (err, summary) {
-                    expect(err).be(null);
-                    expect(summary.run.error).be(null);
-                    expect(summary.run.failures.length).to.be(2);
                     next();
                 }).not.throwException();
             }
