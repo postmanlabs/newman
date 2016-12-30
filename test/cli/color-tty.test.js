28aed9ae38868ed9044225602fca5c82ebbd4eed
Kunal Nagpal <kunagpal@gmail.com>
2016-12-30 13:09:44 +0530

    Test state consistency improvements

--- test/cli/color-tty.test.js
+++ test/cli/color-tty.test.js
@@ -38,23 +38,37 @@ describe('CLI output', function () {
     });
 
     describe('noTTY', function () {
-        var encoding = 'utf-8';
+        var encoding = 'utf-8',
+            outFile = 'out/color-tty-test.txt';
 
-        before(function (done) {
-            fs.mkdir('out', function (err) {
-                console.error(err);
+        beforeEach(function (done) {
+            fs.stat('out', function (err) {
+                if (err) {
+                    return fs.mkdir('out', done);
+                }
+
                 done();
             });
         });
 
+        afterEach(function (done) {
+            fs.stat(outFile, function (err) {
+                if (err) {
+                    return done();
+                }
+
+                fs.unlink(outFile, done);
+            });
+        });
+
         it('should not produce colored output without any options', function (done) {
-            exec('node ./bin/newman.js run test/cli/single-get-request.json > out/notty.txt',
+            exec(`node ./bin/newman.js run test/cli/single-get-request.json > ${outFile}`,
             function (code, stdout, stderr) {
                 expect(code).be(0);
                 expect(stderr).to.be.empty();
                 expect(stdout).to.be.empty();
 
-                fs.readFile('out/notty.txt', encoding, function (err, data) {
+                fs.readFile(outFile, encoding, function (err, data) {
                     if (err) { return done(err); }
 
                     expect(data).to.not.match(coloredOutput);
@@ -64,13 +78,13 @@ describe('CLI output', function () {
         });
 
         it('should produce colored output with --color', function (done) {
-            exec('node ./bin/newman.js run test/cli/single-get-request.json --color > out/notty-color.txt',
+            exec(`node ./bin/newman.js run test/cli/single-get-request.json --color > ${outFile}`,
             function (code, stdout, stderr) {
                 expect(code).be(0);
                 expect(stderr).to.be.empty();
                 expect(stdout).to.be.empty();
 
-                fs.readFile('out/notty-color.txt', encoding, function (err, data) {
+                fs.readFile(outFile, encoding, function (err, data) {
                     if (err) { return done(err); }
 
                     expect(data).to.match(coloredOutput);
@@ -80,13 +94,13 @@ describe('CLI output', function () {
         });
 
         it('should not produce colored output with --no-color', function (done) {
-            exec('node ./bin/newman.js run test/cli/single-get-request.json --no-color > out/notty-no-color.txt',
+            exec(`node ./bin/newman.js run test/cli/single-get-request.json --no-color > ${outFile}`,
             function (code, stdout, stderr) {
                 expect(code).be(0);
                 expect(stderr).to.be.empty();
                 expect(stdout).to.be.empty();
 
-                fs.readFile('out/notty-no-color.txt', encoding, function (err, data) {
+                fs.readFile(outFile, encoding, function (err, data) {
                     if (err) { return done(err); }
 
                     expect(data).to.not.match(coloredOutput);
