ab34454ec072b2b23bddab55336328a67ad54eed
Kunal Nagpal <kunagpal@gmail.com>
2017-10-13 16:15:29 +0530

    Updated NSP config tests

--- test/system/nsp.test.js
+++ test/system/nsp.test.js
@@ -31,7 +31,7 @@ describe('nsp', function () {
 
         it('must exclude only a known set of packages (prevent erroneous exclusions)', function () {
             expect(nsprc.exclusions).to.eql({
-                'postman-runtime': '6.4.0',
+                'postman-runtime': '6.4.1',
                 'postman-collection': '3.0.1'
             });
         });
