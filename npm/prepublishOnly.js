0806d53488f6ab7f75352272ccc2ddebf563faeb
deepakpathania <deepakpathania789@gmail.com>
2017-11-21 18:01:48 +0530

    Script made require friendly, success message added

--- npm/prepublishOnly.js
+++ npm/prepublishOnly.js
@@ -3,24 +3,41 @@
 // This script is intended to create man page for this module
 // ---------------------------------------------------------------------------------------------------------------------
 
-process.chdir(__dirname);
+require('shelljs/global');
 
 var fs = require('fs'),
-    readmeToManPage = require('readme-to-man-page'),
     path = require('path'),
-    readme = fs.readFileSync(path.join(process.cwd(), '../README.md'), 'utf8'),
-    pkg = require(path.join(process.cwd(), '../package.json')),
+
+    readme = fs.readFileSync(path.join(__dirname, '../README.md'), 'utf8'),
+    pkg = require(path.join(__dirname, '../package.json')),
+
+    colors = require('colors/safe'),
+    readmeToManPage = require('readme-to-man-page'),
+
     manPath,
     manPageContent;
 
-manPath = path.resolve(path.join(process.cwd(), '../man/newman.1'));
+manPath = path.resolve(path.join(__dirname, '../man/newman.1'));
 
-manPageContent = readmeToManPage(readme, {
-    name: pkg.name,
-    version: pkg.version,
-    description: pkg.description,
-    section: '1',
-    manual: 'Newman User Manual'
-});
+module.exports = function (exit) {
 
-fs.writeFileSync(manPath, manPageContent, 'utf-8');
+    // create the man page content from markdown
+    manPageContent = readmeToManPage(readme, {
+        name: pkg.name,
+        version: pkg.version,
+        description: pkg.description,
+        section: '1',
+        manual: 'Newman User Manual'
+    });
+
+    // write the generated content to the specified path
+    fs.writeFile(manPath, manPageContent, function (err) {
+        console.info(err ? err : colors.green.bold(`- man page generated at "${manPath}"`));
+        exit(err ? 1 : 0);
+    });
+};
+
+// ensure we run this script exports if this is a direct stdin.tty run
+!module.parent && module.exports(exit);
+
+
