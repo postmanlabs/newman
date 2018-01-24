e6d1d1f969457b2bfbdec4c5ff570024ded90321
Rishi Vikram <rishiv30@gmail.com>
2018-01-25 01:08:49 +0530

    Added the main script that runs after successful tag build on newman, and pushes the image on docker hub

---
+++ docker/scripts/run.sh
@@ -0,0 +1,32 @@
+function build_docker_image {
+    BASENAME=$(basename $1)
+    IMAGE_NAME="newman_$BASENAME"
+	docker build -t "$DOCKER_ID_USER/$IMAGE_NAME:$TRAVIS_TAG" .
+	if docker images | grep -q "$DOCKER_ID_USER/$IMAGE_NAME"; then
+		echo "Image built"
+		if docker run -t "$DOCKER_ID_USER/$IMAGE_NAME:$TRAVIS_TAG" run "https://www.getpostman.com/collections/8a0c9bc08f062d12dcda"; then
+			echo "Collection run successfully"
+			docker login -u "$DOCKER_ID_USER" -p "$DOCKER_ID_PASSWORD"
+			docker push "$DOCKER_ID_USER/$IMAGE_NAME:$TRAVIS_TAG"
+		else
+			echo "Collection not run successfully"
+		fi
+	else
+		echo "Image not built"
+	fi
+}
+
+echo "$TRAVIS_TAG"
+NODE_V=$(node -v | grep -o "v.")
+echo "$NODE_V"
+
+IMAGES_BASE_PATH="./docker/images"
+if [ -n "$TRAVIS_TAG" ] && [ "$NODE_V" = "v8" ]; then
+	for image in $IMAGES_BASE_PATH/*; do
+	    if [ -d "${image}" ] && [ -f "${image}/Dockerfile" ]; then
+	    	cp "${image}/Dockerfile" .
+	        build_docker_image ${image};
+	        rm -f ./Dockerfile
+	    fi
+    done
+fi
