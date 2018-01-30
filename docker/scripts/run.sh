d7144121637b3e2fdd4ef33772da4d84bca57aa5
Rishi Vikram <rishiv30@gmail.com>
2018-01-30 12:52:38 +0530

    Updated dockerfiles and script so that newman version to be installed is taken from package.json after it has been released

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -1,28 +1,33 @@
+#!/bin/bash
 function build_docker_image {
     BASENAME=$(basename $1);
+    TAG="$2";
     IMAGE_NAME="newman_$BASENAME";
-    docker build -t "$DOCKER_ID_USER/$IMAGE_NAME:$TRAVIS_TAG" -t "$DOCKER_ID_USER/$IMAGE_NAME:latest" .;
+    docker build -t "$DOCKER_ID_USER/$IMAGE_NAME:$TAG" -t "$DOCKER_ID_USER/$IMAGE_NAME:latest" --build-arg newman_version="$TAG" .;
     if docker images | grep -q "$DOCKER_ID_USER/$IMAGE_NAME"; then
         echo "Image built";
-        if docker run -t "$DOCKER_ID_USER/$IMAGE_NAME:$TRAVIS_TAG" run "sample-collection.json"; then
+        PWD=$(pwd);
+        if docker run -v "$PWD/examples:/etc/newman" -t "$DOCKER_ID_USER/$IMAGE_NAME:$TAG" run "sample-collection.json"; then
             echo "Collection run successfully";
             docker login -u "$DOCKER_ID_USER" -p "$DOCKER_ID_PASSWORD";
-            docker push "$DOCKER_ID_USER/$IMAGE_NAME:$TRAVIS_TAG" "$DOCKER_ID_USER/$IMAGE_NAME:latest";
+            docker push "$DOCKER_ID_USER/$IMAGE_NAME:$TAG" "$DOCKER_ID_USER/$IMAGE_NAME:latest";
         else
             echo "Collection not run successfully";
         fi
     else
-        echo "Image $BASENAME:$TRAVIS_TAG and $BASENAME:latest not built";
+        echo "Image $BASENAME:$TAG and $BASENAME:latest not built";
     fi
 }
-
 IMAGES_BASE_PATH="./docker/images";
-EXTRACTED_TAG=$(echo $TRAVIS_TAG | grep -o "[0-9]\+.[0-9]\+.[0-9]\+");
-if [ -n "$TRAVIS_TAG" ] && [ "$TRAVIS_TAG" = "$EXTRACTED_TAG" ] && [ "$TRAVIS_NODE_VERSION" = "8" ]; then
+TAG="$npm_package_version";
+if [ -z "$TAG" ]; then
+    TAG=$(grep "\"version\":\ " package.json | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+");
+fi
+if [ -n "$TAG" ]; then
     for image in $IMAGES_BASE_PATH/*; do
         if [ -f "${image}/Dockerfile" ]; then
             cp "${image}/Dockerfile" .;
-            build_docker_image ${image};
+            build_docker_image ${image} ${TAG};
             rm -f ./Dockerfile;
         fi
     done
