da2e1ecb3d837de721a7eacd5912afa0bd1b7dec
Kunal Nagpal <kunagpal@gmail.com>
2018-05-08 15:36:32 +0530

    Cleaned up postpublish script :art:
    * Added validation, coloured output
    * Added steps to autopublish the built images
    * Updated docker build command to work without caching, remove intermediate image layers

--- npm/postpublish.sh
+++ npm/postpublish.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
-function build_docker_image {
-    BASENAME=$(basename $1);
-    TAG="$2";
-    IMAGE_NAME="newman_$BASENAME";
-    docker build -t "$DOCKER_USER/$IMAGE_NAME:$TAG" -t "$DOCKER_USER/$IMAGE_NAME:latest" --build-arg newman_version="$TAG" .;
-    if docker images | grep -q "$DOCKER_USER/$IMAGE_NAME"; then
-        echo "Image built";
-        PWD=$(pwd);
-        if docker run -v "$PWD/examples:/etc/newman" -t "$DOCKER_USER/$IMAGE_NAME:$TAG" run "sample-collection.json"; then
-            echo "Collection run successfully";
-            docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD";
-            docker push "$DOCKER_USER/$IMAGE_NAME:$TAG" "$DOCKER_USER/$IMAGE_NAME:latest";
-        else
-            echo "Collection not run successfully";
-        fi
-    else
-        echo "Image $BASENAME:$TAG and $BASENAME:latest not built";
-    fi
-}
+
+# Bail out on the first error
+set -e;
+
+BLUE='\033[0;34m';
+NO_COLOUR='\033[0m';
+DOCKER_USER="postman";
+TAG=$npm_package_version;
 IMAGES_BASE_PATH="./docker/images";
-TAG="$npm_package_version";
-if [ -z "$TAG" ]; then
-    TAG=$(grep "\"version\":\ " package.json | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+");
-fi
-if [ -n "$TAG" ]; then
-    for image in $IMAGES_BASE_PATH/*; do
-        if [ -f "${image}/Dockerfile" ]; then
-            cp "${image}/Dockerfile" .;
-            build_docker_image ${image} ${TAG};
-            rm -f ./Dockerfile;
-        fi
-    done
-fi
+
+# It's good to be paranoid
+[[ -z "$TAG" ]] && TAG=$(jq -r ".version" < package.json);
+
+function build_docker_image {
+    local TAG="$2";
+    local BASENAME=$(basename $1);
+    local IMAGE_NAME="newman_$BASENAME";
+
+    echo "";
+    echo -e "$BLUE Building docker image for $DOCKER_USER/$IMAGE_NAME:$TAG, latest $NO_COLOUR";
+    docker build \
+        -t "$DOCKER_USER/$IMAGE_NAME:$TAG" -t "$DOCKER_USER/$IMAGE_NAME:latest" \
+        --no-cache --force-rm --file="docker/images/$BASENAME/Dockerfile" --build-arg newman_version="$TAG" .;
+
+    echo -e "$BLUE Running docker image test for $DOCKER_USER/$IMAGE_NAME:$TAG, latest $NO_COLOUR";
+    docker run -v "$PWD/examples:/etc/newman" -t "$DOCKER_USER/$IMAGE_NAME:$TAG" run "sample-collection.json";
+
+    # prepare current images for pushing
+    docker tag $DOCKER_USER/$IMAGE_NAME:$TAG $DOCKER_USER/$IMAGE_NAME:$TAG;
+    docker tag $DOCKER_USER/$IMAGE_NAME:latest $DOCKER_USER/$IMAGE_NAME:latest;
+
+    echo -e "$BLUE Pushing docker image for $DOCKER_USER/$IMAGE_NAME:$TAG, latest $NO_COLOUR";
+    docker push $DOCKER_USER/$IMAGE_NAME:$TAG;
+    docker push $DOCKER_USER/$IMAGE_NAME:latest;
+}
+
+for image in $IMAGES_BASE_PATH/*; do
+    build_docker_image ${image} ${TAG};
+done
