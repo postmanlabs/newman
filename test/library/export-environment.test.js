4e2f1594aa7a08045f8895f1480c89871597c834
Kunal Nagpal <kunagpal@gmail.com>
2018-06-01 23:58:26 +0530

    Added environment export library tests

--- test/library/export-environment.test.js
+++ test/library/export-environment.test.js
@@ -71,4 +71,63 @@ describe('--export-environment', function () {
             done();
         });
     });
+
+    it('`newman run` should export env with a name if the input file doesn\'t have one', function (done) {
+        newman.run({
+            collection: 'test/fixtures/run/single-request-failing.json',
+            environment: {
+                values: [{ key: 'var-1', value: 'value-1' }]
+            },
+            exportEnvironment: exportedEnvironmentPath
+        }, function (err) {
+            if (err) { return done(err); }
+
+            var environment;
+
+            try { environment = JSON.parse(fs.readFileSync(exportedEnvironmentPath).toString()); }
+            catch (e) { console.error(e); }
+
+            expect(environment).be.ok();
+            expect(environment).have.property('_postman_exported_at');
+            expect(environment).have.property('values');
+            expect(environment).have.property('name', 'environment');
+            expect(environment.values).eql([
+                { key: 'var-1', value: 'value-1', type: 'any' }
+            ]);
+            expect(environment).have.property('_postman_variable_scope', 'environment');
+            done();
+        });
+    });
+
+    it('`newman run` should export environment with a name when no input file is provided', function (done) {
+        newman.run({
+            collection: {
+                item: [{
+                    event: [{
+                        listen: 'test',
+                        script: 'pm.environment.set("var-1", "value-1");'
+                    }],
+                    request: 'https://postman-echo.com/get?source=newman-sample-github-collection'
+                }]
+            },
+            exportEnvironment: exportedEnvironmentPath
+        }, function (err) {
+            if (err) { return done(err); }
+
+            var environment;
+
+            try { environment = JSON.parse(fs.readFileSync(exportedEnvironmentPath).toString()); }
+            catch (e) { console.error(e); }
+
+            expect(environment).be.ok();
+            expect(environment).have.property('_postman_exported_at');
+            expect(environment).have.property('values');
+            expect(environment).have.property('name', 'environment');
+            expect(environment.values).eql([
+                { key: 'var-1', value: 'value-1', type: 'any' }
+            ]);
+            expect(environment).have.property('_postman_variable_scope', 'environment');
+            done();
+        });
+    });
 });
