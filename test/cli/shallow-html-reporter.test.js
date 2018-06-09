f7a6d6d516432ac7d2df0cc91bfe659b7bc1e2d5
Kunal Nagpal <kunagpal@gmail.com>
2018-06-09 16:40:04 +0530

    Added reporter CLI tests :white_check_mark:

--- test/cli/shallow-html-reporter.test.js
+++ test/cli/shallow-html-reporter.test.js
@@ -1,26 +1,18 @@
-var fs = require('fs');
+var fs = require('fs'),
+
+    sh = require('shelljs');
 
 describe('HTML reporter', function () {
-    var outFile = 'out/newman-report.html';
+    var outDir = 'out',
+        outFile = outDir + '/newman-report.html';
 
-    beforeEach(function (done) {
-        fs.stat('out', function (err) {
-            if (err) {
-                return fs.mkdir('out', done);
-            }
-
-            done();
-        });
+    beforeEach(function () {
+        sh.test('-d', outDir) && sh.rm('-rf', outDir);
+        sh.mkdir('-p', outDir);
     });
 
-    afterEach(function (done) {
-        fs.stat(outFile, function (err) {
-            if (err) {
-                return done();
-            }
-
-            fs.unlink(outFile, done);
-        });
+    afterEach(function () {
+        sh.rm('-rf', outDir);
     });
 
     it('should correctly generate the html report for a successful run', function (done) {
@@ -56,6 +48,20 @@ describe('HTML reporter', function () {
             function (code) {
                 expect(code).be(1);
                 fs.stat(outFile, done);
+            });
+    });
+
+    it('should correctly produce the html report in a pre-existing directory', function (done) {
+        // eslint-disable-next-line max-len
+        exec('node ./bin/newman.js run test/fixtures/run/single-get-request.json -r html --reporter-html-export out',
+            function (code) {
+                expect(code).be(0);
+
+                var dir = fs.readdirSync(outDir),
+                    file = dir[0];
+
+                expect(dir).to.have.property('length', 1);
+                fs.stat(outDir + '/' + file, done);
             });
     });
 });
