fa6a4bbecf30a612eaf35175e4a34a2ca0fa8f16
Udit Vasu <codenirvana28@gmail.com>
2018-06-18 20:24:20 +0530

    Update Travis tests to chai

--- test/system/travis-yml.test.js
+++ test/system/travis-yml.test.js
@@ -1,5 +1,5 @@
 /* global describe, it */
-var expect = require('expect.js');
+var expect = require('chai').expect;
 
 describe('travis.yml', function () {
     var fs = require('fs'),
@@ -14,17 +14,17 @@ describe('travis.yml', function () {
         travisYAMLError = e;
     }
 
-    it('must exist', function (done) {
+    it('should exist', function (done) {
         fs.stat('.travis.yml', done);
     });
 
-    it('must be a valid yml', function () {
-        expect(travisYAMLError && travisYAMLError.message || travisYAMLError).to.not.be.ok();
+    it('should be a valid yml', function () {
+        expect(travisYAMLError && travisYAMLError.message || travisYAMLError).to.not.be.ok;
     });
 
     describe('strucure', function () {
-        it('language must be set to node', function () {
-            expect(travisYAML.language).to.be('node_js');
+        it('should have language to be set to node', function () {
+            expect(travisYAML.language).to.equal('node_js');
             expect(travisYAML.node_js).to.eql(['4', '6', '8']);
         });
     });
