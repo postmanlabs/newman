079f3930f4a22c3fec8d777bf56165a5168fb288
Udit Vasu <codenirvana28@gmail.com>
2018-06-18 20:19:00 +0530

    Update nsp tests to chai

--- test/system/nsp.test.js
+++ test/system/nsp.test.js
@@ -2,7 +2,7 @@
  * @fileOverview Ensures nsprc is as expected
  */
 
-var expect = require('expect.js'),
+var expect = require('chai').expect,
     fs = require('fs'),
     _ = require('lodash');
 
@@ -16,31 +16,31 @@ describe('nsp', function () {
         pkg = JSON.parse(fs.readFileSync('./package.json').toString());
     });
 
-    it('must be a dev dependency', function () {
-        expect(pkg.devDependencies && pkg.devDependencies.nsp).to.be.ok();
+    it('should be a dev dependency', function () {
+        expect(pkg.devDependencies && pkg.devDependencies.nsp).to.be.ok;
     });
 
     describe('nsprc', function () {
-        it('must exist', function () {
-            expect(nsprc).to.be.ok();
+        it('should exist', function () {
+            expect(nsprc).to.be.ok;
         });
 
-        it('must not have any exclusion', function () {
+        it('should not have any exclusion', function () {
             expect(nsprc.exceptions).to.eql([]);
         });
 
-        it('must not have exclusions (prevent erroneous exclusions)', function () {
+        it('should not have exclusions (prevent erroneous exclusions)', function () {
             expect(nsprc.exclusions).to.eql({});
         });
 
-        it('dependency version in package.json should match .nsprc (time to remove exclusion?)', function () {
+        it('should match dependency version in package.json to .nsprc (time to remove exclusion?)', function () {
             var pkg = _.pick(require('../../package').dependencies, _.keys(nsprc.exclusions));
             expect(pkg).to.eql(nsprc.exclusions);
         });
 
         // if you are changing the version here, most probably you are better of removing the exclusion in first place.
         // remove the exclusion and check if nsp passes, else update the version here
-        it('on excluded package\'s version change must reconsider removing exclusion', function () {
+        it('should reconsider removing exclusion on excluded package\'s version change', function () {
             // expect(pkg.dependencies).to.have.property('<name>', '<semver>');
         });
     });
