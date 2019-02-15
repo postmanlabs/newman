21efc0bc3e4be7f1822c42d135fcfc1cc1687e64
vikiCoder <premangvikani@gmail.com>
2019-02-15 11:37:25 +0530

    bumped up versions of postman-runtime, postman-collection and postman-request

--- test/cli/verbose.test.js
+++ test/cli/verbose.test.js
@@ -2,18 +2,14 @@ var _ = require('lodash');
 
 describe('newman run --verbose', function () {
     var verboseStrings = [
-        'Wait',
-        'DNS lookup',
-        'TCP handshake',
-        'SSL handshake',
-        'Transfer start',
-        'Download',
-        'Total',
-        'average wait time:',
+        'wait',
+        'dns-lookup',
+        'tcp-handshake',
+        'ssl-handshake',
+        'transfer-start',
+        'download',
         'average DNS lookup time:',
-        'average TCP connection time:',
-        'average first byte time:',
-        'average download time:'
+        'average first byte time:'
     ];
 
     it('should include verbose with --verbose option', function (done) {
