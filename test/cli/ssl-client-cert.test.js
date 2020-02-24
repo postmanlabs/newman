a94764658c6e412bb20f6932d129da3d6d2f6d6f
Utkarsh Maheshwari <coditva@gmail.com>
2020-02-24 17:41:00 +0530

    Fix incorrect calling of `done` in tests

--- test/cli/ssl-client-cert.test.js
+++ test/cli/ssl-client-cert.test.js
@@ -1,68 +1,78 @@
 var fs = require('fs'),
+    async = require('async'),
     https = require('https');
 
 describe('SSL Client certificates', function () {
     var server1, server2, server3;
 
+    function createHttpsServerWithCerts (certs) {
+        return https.createServer({
+            key: fs.readFileSync(certs.key, 'utf8'),
+            cert: fs.readFileSync(certs.cert, 'utf8'),
+            ca: fs.readFileSync(certs.ca, 'utf8'),
+            passphrase: 'password',
+            requestCert: true,
+            rejectUnauthorized: false
+        }, function (req, res) {
+            if (req.client.authorized) {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end('authorized\n');
+            }
+            else {
+                res.writeHead(401, { 'Content-Type': 'text/plain' });
+                res.end('unauthorized\n');
+            }
+        });
+    }
+
     before(function (done) {
-        server1 = https.createServer({
-            key: fs.readFileSync('test/fixtures/ssl/server.key', 'utf8'),
-            cert: fs.readFileSync('test/fixtures/ssl/server.crt', 'utf8'),
-            ca: fs.readFileSync('test/fixtures/ssl/ca.crt', 'utf8'),
-            passphrase: 'password',
-            requestCert: true,
-            rejectUnauthorized: false
-        }, function (req, res) {
-            if (req.client.authorized) {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end('authorized\n');
-            }
-            else {
-                res.writeHead(401, { 'Content-Type': 'text/plain' });
-                res.end('unauthorized\n');
-            }
+        server1 = createHttpsServerWithCerts({
+            key: 'test/fixtures/ssl/server.key',
+            cert: 'test/fixtures/ssl/server.crt',
+            ca: 'test/fixtures/ssl/ca.crt'
         });
 
-        server2 = https.createServer({
-            key: fs.readFileSync('test/fixtures/ssl/server2.key', 'utf8'),
-            cert: fs.readFileSync('test/fixtures/ssl/server2.crt', 'utf8'),
-            ca: fs.readFileSync('test/fixtures/ssl/ca2.crt', 'utf8'),
-            passphrase: 'password',
-            requestCert: true,
-            rejectUnauthorized: false
-        }, function (req, res) {
-            if (req.client.authorized) {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end('authorized\n');
-            }
-            else {
-                res.writeHead(401, { 'Content-Type': 'text/plain' });
-                res.end('unauthorized\n');
-            }
+        server2 = createHttpsServerWithCerts({
+            key: 'test/fixtures/ssl/server2.key',
+            cert: 'test/fixtures/ssl/server2.crt',
+            ca: 'test/fixtures/ssl/ca2.crt'
         });
 
-        server3 = https.createServer({
-            key: fs.readFileSync('test/fixtures/ssl/server3.key', 'utf8'),
-            cert: fs.readFileSync('test/fixtures/ssl/server3.crt', 'utf8'),
-            ca: fs.readFileSync('test/fixtures/ssl/ca3.crt', 'utf8'),
-            passphrase: 'password',
-            requestCert: true,
-            rejectUnauthorized: false
-        }, function (req, res) {
-            if (req.client.authorized) {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end('authorized\n');
-            }
-            else {
-                res.writeHead(401, { 'Content-Type': 'text/plain' });
-                res.end('unauthorized\n');
-            }
+        server3 = createHttpsServerWithCerts({
+            key: 'test/fixtures/ssl/server3.key',
+            cert: 'test/fixtures/ssl/server3.crt',
+            ca: 'test/fixtures/ssl/ca3.crt'
         });
 
-        server1.listen(3000);
-        server2.listen(3001);
-        server3.listen(3002);
-        done();
+        async.parallel([
+            function (cb) {
+                server1.listen(3000, cb);
+            },
+            function (cb) {
+                server2.listen(3001, cb);
+            },
+            function (cb) {
+                server3.listen(3002, cb);
+            }
+        ], function (err) {
+            done(err);
+        });
+    });
+
+    after(function (done) {
+        async.parallel([
+            function (cb) {
+                server1.close(cb);
+            },
+            function (cb) {
+                server2.close(cb);
+            },
+            function (cb) {
+                server3.close(cb);
+            }
+        ], function (err) {
+            done(err);
+        });
     });
 
     // @todo: add .pfx, .pem tests as well
@@ -140,12 +150,5 @@ describe('SSL Client certificates', function () {
             expect(code, 'should not have exit code 0').to.not.equal(0);
             done();
         });
-    });
-
-    after(function (done) {
-        server1.close();
-        server2.close();
-        server3.close();
-        done();
     });
 });
