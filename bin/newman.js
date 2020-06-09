1c604d9feaa767f13644328de09db2fd0434894d
Rohit Gohri <gohri.rohit@gmail.com>
2020-06-09 14:31:35 +0530

    Add cookieJar option for #918

--- bin/newman.js
+++ bin/newman.js
@@ -65,6 +65,8 @@ program
         'Specify the Client SSL passphrase (optional, needed for passphrase protected keys).')
     .option('--ssl-extra-ca-certs <path>',
         'Specify additionally trusted CA certificates')
+    .option('--cookie-jar <path>',
+        'Specify custom cookie jar file path')
     .option('--verbose', 'Show detailed information of collection run and each request sent')
     .action((collection, command) => {
         let options = util.commanderToObject(command),
