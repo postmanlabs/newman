52b645e57b0df1eb9cb8b098a3dbd1a1ed6be884
Udit Vasu <udit@postman.com>
2020-08-30 11:18:39 +0530

    Update CLI options description

--- bin/newman.js
+++ bin/newman.js
@@ -18,56 +18,48 @@ program
 // The `run` command allows you to specify a collection to be run with the provided options.
 program
     .command('run <collection>')
-    .description('URL or path to a Postman Collection.')
+    .description('URL or path to a Postman Collection')
     .usage('<collection> [options]')
-    .option('-e, --environment <path>', 'Specify a URL or Path to a Postman Environment.')
-    .option('-g, --globals <path>', 'Specify a URL or Path to a file containing Postman Globals.')
-    // eslint-disable-next-line max-len
-    .option('--folder <path>', 'Specify folder to run from a collection. Can be specified multiple times to run multiple folders', util.cast.memoize, [])
-    .option('--working-dir <path>', 'The path of the directory to be used as the working directory')
-    .option('--no-insecure-file-read', 'Prevents reading the files situated outside of the working directory')
-    .option('-r, --reporters [reporters]', 'Specify the reporters to use for this run.', util.cast.csvParse, ['cli'])
-    .option('-n, --iteration-count <n>', 'Define the number of iterations to run.', util.cast.integer)
-    .option('-d, --iteration-data <path>', 'Specify a data file to use for iterations (either json or csv).')
-    .option('--export-environment <path>', 'Exports the environment to a file after completing the run.')
-    .option('--export-globals <path>', 'Specify an output file to dump Globals before exiting.')
-    .option('--export-collection <path>', 'Specify an output file to save the executed collection')
-    .option('--postman-api-key <apiKey>', 'API Key used to load the resources from the Postman API.')
-    .option('--delay-request [n]', 'Specify the extent of delay between requests (milliseconds)', util.cast.integer, 0)
-    .option('--bail [modifiers]',
-        'Specify whether or not to gracefully stop a collection run on encountering an error' +
-        'and whether to end the run with an error based on the optional modifier.', util.cast.csvParse)
-    .option('-x , --suppress-exit-code',
-        'Specify whether or not to override the default exit code for the current run.')
-    .option('--silent', 'Prevents newman from showing output to CLI.')
-    .option('--disable-unicode',
-        'Forces unicode compliant symbols to be replaced by their plain text equivalents')
+    .option('-e, --environment <path>', 'Specify a URL or path to a Postman Environment')
+    .option('-g, --globals <path>', 'Specify a URL or path to a file containing Postman Globals')
+    .option('-r, --reporters [reporters]', 'Specify the reporters to use for this run', util.cast.csvParse, ['cli'])
+    .option('-n, --iteration-count <n>', 'Define the number of iterations to run', util.cast.integer)
+    .option('-d, --iteration-data <path>', 'Specify a data file to use for iterations (either JSON or CSV)')
+    .option('--folder <path>',
+        'Specify the folder to run from a collection. Can be specified multiple times to run multiple folders',
+        util.cast.memoize, [])
     .option('--global-var <value>',
         'Allows the specification of global variables via the command line, in a key=value format',
         util.cast.memoizeKeyVal, [])
     .option('--env-var <value>',
         'Allows the specification of environment variables via the command line, in a key=value format',
         util.cast.memoizeKeyVal, [])
-    .option('--color <value>', 'Enable/Disable colored output. (auto|on|off)', util.cast.colorOptions, 'auto')
-    .option('--timeout [n]', 'Specify a timeout for collection run (in milliseconds)', util.cast.integer, 0)
-    .option('--timeout-request [n]', 'Specify a timeout for requests (in milliseconds).', util.cast.integer, 0)
-    .option('--timeout-script [n]', 'Specify a timeout for script (in milliseconds).', util.cast.integer, 0)
-    .option('--ignore-redirects', 'If present, Newman will not follow HTTP Redirects.')
-    .option('-k, --insecure', 'Disables SSL validations.')
-    .option('--ssl-client-cert-list <path>',
-        'Specify the path to the Client SSL certificates configuration file to set certificates' +
-        ' per URL/hostname (json).')
-    .option('--ssl-client-cert <path>',
-        'Specify the path to the Client SSL certificate. Supports .cert and .pfx files.')
-    .option('--ssl-client-key <path>',
-        'Specify the path to the Client SSL key (not needed for .pfx files)')
-    .option('--ssl-client-passphrase <passphrase>',
-        'Specify the Client SSL passphrase (optional, needed for passphrase protected keys).')
-    .option('--ssl-extra-ca-certs <path>',
-        'Specify additionally trusted CA certificates')
-    .option('--cookie-jar <path>', 'Specify custom cookie jar file path (serialized tough-cookie JSON) ')
-    .option('--export-cookie-jar <path>',
-        'Exports the cookie jar to a file after completing the run.')
+    .option('--export-environment <path>', 'Exports the final environment to a file after completing the run')
+    .option('--export-globals <path>', 'Exports the final globals to a file after completing the run')
+    .option('--export-collection <path>', 'Exports the executed collection to a file after completing the run')
+    .option('--postman-api-key <apiKey>', 'API Key used to load the resources from the Postman API')
+    .option('--bail [modifiers]',
+        'Specify whether or not to gracefully stop a collection run on encountering an error' +
+        ' and whether to end the run with an error based on the optional modifier', util.cast.csvParse)
+    .option('--ignore-redirects', 'Prevents Newman from automatically following 3XX redirect responses')
+    .option('-x , --suppress-exit-code', 'Specify whether or not to override the default exit code for the current run')
+    .option('--silent', 'Prevents Newman from showing output to CLI')
+    .option('--disable-unicode', 'Forces Unicode compliant symbols to be replaced by their plain text equivalents')
+    .option('--color <value>', 'Enable/Disable colored output (auto|on|off)', util.cast.colorOptions, 'auto')
+    .option('--delay-request [n]', 'Specify the extent of delay between requests (milliseconds)', util.cast.integer, 0)
+    .option('--timeout [n]', 'Specify a timeout for collection run (milliseconds)', util.cast.integer, 0)
+    .option('--timeout-request [n]', 'Specify a timeout for requests (milliseconds)', util.cast.integer, 0)
+    .option('--timeout-script [n]', 'Specify a timeout for scripts (milliseconds)', util.cast.integer, 0)
+    .option('--working-dir <path>', 'Specify the path to the working directory')
+    .option('--no-insecure-file-read', 'Prevents reading the files situated outside of the working directory')
+    .option('-k, --insecure', 'Disables SSL validations')
+    .option('--ssl-client-cert-list <path>', 'Specify the path to a client certificates configurations (JSON)')
+    .option('--ssl-client-cert <path>', 'Specify the path to a client certificate (PEM)')
+    .option('--ssl-client-key <path>', 'Specify the path to a client certificate private key')
+    .option('--ssl-client-passphrase <passphrase>', 'Specify the client certificate passphrase (for protected key)')
+    .option('--ssl-extra-ca-certs <path>', 'Specify additionally trusted CA certificates (PEM)')
+    .option('--cookie-jar <path>', 'Specify the path to a custom cookie jar (serialized tough-cookie JSON) ')
+    .option('--export-cookie-jar <path>', 'Exports the cookie jar to a file after completing the run')
     .option('--verbose', 'Show detailed information of collection run and each request sent')
     .action((collection, command) => {
         let options = util.commanderToObject(command),
