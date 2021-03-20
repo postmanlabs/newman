e69e9e33b04f2a0f24d885b9537456a093e4d9ce
raghavbhatia336 <raghavbhatia05@gmail.com>
2021-03-20 07:25:57 +0530

    Add request function, Move curl-to-postman to configLoaders

--- bin/newman.js
+++ bin/newman.js
@@ -112,42 +112,8 @@ program
         'Forces the request to be sent as HEAD, with the --data parameters appended to the query string')
     .option('-T, --upload-file <file>',
         'Forces the request to be sent as PUT with the specified local file to the server')
-    .option('-e, --environment <path>', 'Specify a URL or path to a Postman Environment')
-    .option('-g, --globals <path>', 'Specify a URL or path to a file containing Postman Globals')
     .option('-r, --reporters [reporters]', 'Specify the reporters to use for this run', util.cast.csvParse, ['cli'])
-    .option('-n, --iteration-count <n>', 'Define the number of iterations to run', util.cast.integer)
-    .option('--global-var <value>',
-        'Allows the specification of global variables via the command line, in a key=value format',
-        util.cast.memoizeKeyVal, [])
-    .option('--env-var <value>',
-        'Allows the specification of environment variables via the command line, in a key=value format',
-        util.cast.memoizeKeyVal, [])
-    .option('--export-environment <path>', 'Exports the final environment to a file after completing the run')
-    .option('--export-globals <path>', 'Exports the final globals to a file after completing the run')
-    .option('--export-collection <path>', 'Exports the executed collection to a file after completing the run')
-    .option('--postman-api-key <apiKey>', 'API Key used to load the resources from the Postman API')
-    .option('--bail [modifiers]',
-        'Specify whether or not to gracefully stop a collection run on encountering an error' +
-        ' and whether to end the run with an error based on the optional modifier', util.cast.csvParse)
-    .option('--ignore-redirects', 'Prevents Newman from automatically following 3XX redirect responses')
-    .option('-x , --suppress-exit-code', 'Specify whether or not to override the default exit code for the current run')
-    .option('--silent', 'Prevents Newman from showing output to CLI')
-    .option('--disable-unicode', 'Forces Unicode compliant symbols to be replaced by their plain text equivalents')
     .option('--color <value>', 'Enable/Disable colored output (auto|on|off)', util.cast.colorOptions, 'auto')
-    .option('--delay-request [n]', 'Specify the extent of delay between requests (milliseconds)', util.cast.integer, 0)
-    .option('--timeout [n]', 'Specify a timeout for collection run (milliseconds)', util.cast.integer, 0)
-    .option('--timeout-request [n]', 'Specify a timeout for requests (milliseconds)', util.cast.integer, 0)
-    .option('--timeout-script [n]', 'Specify a timeout for scripts (milliseconds)', util.cast.integer, 0)
-    .option('--working-dir <path>', 'Specify the path to the working directory')
-    .option('--no-insecure-file-read', 'Prevents reading the files situated outside of the working directory')
-    .option('-k, --insecure', 'Disables SSL validations')
-    .option('--ssl-client-cert-list <path>', 'Specify the path to a client certificates configurations (JSON)')
-    .option('--ssl-client-cert <path>', 'Specify the path to a client certificate (PEM)')
-    .option('--ssl-client-key <path>', 'Specify the path to a client certificate private key')
-    .option('--ssl-client-passphrase <passphrase>', 'Specify the client certificate passphrase (for protected key)')
-    .option('--ssl-extra-ca-certs <path>', 'Specify additionally trusted CA certificates (PEM)')
-    .option('--cookie-jar <path>', 'Specify the path to a custom cookie jar (serialized tough-cookie JSON) ')
-    .option('--export-cookie-jar <path>', 'Exports the cookie jar to a file after completing the run')
     .option('--verbose', 'Show detailed information of collection run and each request sent')
     .action((url, command) => {
         let options = util.commanderToObject(command),
@@ -164,7 +130,7 @@ program
             acc[key] = _.assignIn(value, reporterOptions._generic); // overrides reporter options with _generic
         }, {});
 
-        newman.run(options, function (err, summary) {
+        newman.request(options, function (err, summary) {
             const runError = err || summary.run.error || summary.run.failures.length;
 
             if (err) {
