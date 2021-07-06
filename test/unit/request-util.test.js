98c5b826e026da4763e5492bd839f906e8d1e9c3
raghavbhatia336 <raghavbhatia05@gmail.com>
2021-07-06 10:39:43 +0530

    Fixed lint errors in test/unit/request-util.test.js

--- test/unit/request-util.test.js
+++ test/unit/request-util.test.js
@@ -1,33 +1,32 @@
 var util = require('../../lib/request/util.js'),
-    Collection = require('postman-collection').Collection,
-    curl2postman = require('curl-to-postmanv2');
+    Collection = require('postman-collection').Collection;
 
 describe('convert', function () {
-	var result = {
-			result: true,
-    		output:[ { type:"request",
-    				   data:{ method:"GET", 
-    				   	name:"http://google.com", 
-    				 	url:"http://google.com", 
-    				 	header:[], 
-    				 	body:{}, 
-    				 	description:"Generated from a curl request: \ncurl  --get http://google.com" 
-    				   }
-    				 }
-    		]};
+    var result = {
+            result: true,
+            output: [{ type: 'request',
+                data: { method: 'GET',
+                    name: 'http://google.com',
+                    url: 'http://google.com',
+                    header: [],
+                    body: {},
+                    description: 'Generated from a curl request: \ncurl  --get http://google.com'
+                }
+            }
+            ] },
+        value = 'curl  --get http://google.com';
+
 
     let mockCollection = new Collection();
 
     mockCollection.items.add({
-            name: result.output[0] && result.output[0].data.name,
-            request: result.output[0] && result.output[0].data
+        name: result.output[0] && result.output[0].data.name,
+        request: result.output[0] && result.output[0].data
     });
-
-    var value = "curl  --get http://google.com"
 
     const actualCollection = util.convert(value);
 
-	it('should convert curl command to Postman Collection', function () {
+    it('should convert curl command to Postman Collection', function () {
         expect(actualCollection.items.name).to.eql(mockCollection.items.name);
         expect(actualCollection.items.request).to.eql(mockCollection.items.request);
     });
