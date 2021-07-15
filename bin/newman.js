daea00c221550f2d04394a5b701405a264437ab0
raghav <ansraghav100@gmail.com>
2021-07-15 16:41:51 +0530

    Adds comments and fixes variable names

--- bin/newman.js
+++ bin/newman.js
@@ -116,18 +116,20 @@ program
     .action((url, command) => {
         const options = util.commanderToObject(command),
 
+            // convert the commander options object to a curl string
             curl = util.createCurl(options, url);
 
+        // Inject additional properties into the options object
         options.curl = curl;
 
-        newman.request(options, function (err, summary) {
-            const runError = err || summary.run.error || summary.run.failures.length;
+        newman.request(options, function (err) {
+            const requestErr = err;
 
             if (err) {
                 console.error(`error: ${err.message || err}\n`);
                 err.friendly && console.error(`  ${err.friendly}\n`);
             }
-            runError && !_.get(options, 'suppressExitCode') && process.exit(1);
+            requestErr && !_.get(options, 'suppressExitCode') && process.exit(1);
         });
     });
 
