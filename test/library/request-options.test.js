000e27840bc4fcdddc22ee0384eba82577a57a5c
raghavbhatia336 <raghavbhatia05@gmail.com>
2021-07-21 23:02:29 +0530

    Added a library test for single-request

--- test/library/request-options.test.js
+++ test/library/request-options.test.js
@@ -4,4 +4,12 @@ describe('Newman request options', function () {
     it('should work correctly without any extra options', function (done) {
         newman.request({ curl: getRequestCurl }, done);
     });
+
+    it('should not work with empty options', function (done) {
+        newman.request({}, function (err) {
+            expect(err).to.be.ok;
+            expect(err.message).to.eql('expecting a valid curl command to run');
+            done();
+        });
+    });
 });
