bf27111c09be1bcd1b8f90684b4cb4e0ada9acc5
raghavbhatia336 <raghavbhatia05@gmail.com>
2021-07-25 00:47:57 +0530

    Added tests in test/library/request-options.test.js

--- test/library/request-options.test.js
+++ test/library/request-options.test.js
@@ -1,5 +1,6 @@
 describe('Newman request options', function () {
-    var getRequestCurl = 'curl -X GET https://postman-echo.com/get';
+    var getRequestCurl = 'curl -X GET https://postman-echo.com/get',
+        invalidUrl = 'curl -X GET https://123.random.z/get';
 
     it('should work correctly without any extra options', function (done) {
         newman.request({ curl: getRequestCurl }, done);
@@ -9,6 +10,31 @@ describe('Newman request options', function () {
         newman.request({}, function (err) {
             expect(err).to.be.ok;
             expect(err.message).to.eql('expecting a valid curl command to run');
+            done();
+        });
+    });
+
+    it('should execute correct response for correct url', function (done) {
+        newman.request({
+            curl: getRequestCurl
+        }, function (err, summary) {
+            const executions = summary.run.executions,
+                response = executions[0].response.json();
+
+            expect(err).to.not.be.ok;
+            expect(response).to.have.property('url')
+                .that.eql('https://postman-echo.com/get');
+            expect(executions).to.have.lengthOf(1);
+            done();
+        });
+    });
+
+    it('should not execute correct response for incorrect url', function (done) {
+        newman.request({
+            curl: invalidUrl
+        }, function (err, summary) {
+            expect(err).to.not.be.ok;
+            expect(summary.run.failures[0].error.message).to.include('getaddrinfo ENOTFOUND 123.random.z');
             done();
         });
     });
