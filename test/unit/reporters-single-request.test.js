9ee857239da0d0d0c11a80885964473cb5768e85
raghavbhatia336 <raghavbhatia05@gmail.com>
2021-08-18 17:45:44 +0530

    Changed the code in the tests

--- test/unit/reporters-single-request.test.js
+++ test/unit/reporters-single-request.test.js
@@ -7,9 +7,9 @@ describe('Reporter Options', function () {
         });
     });
 
-    it('should correctly work with json reporter options', function (done) {
+    it('should correctly work with progress reporter options', function (done) {
         // eslint-disable-next-line max-len, quotes
-        exec('node ./bin/newman.js request -X GET "https://5a178277-8664-409c-8e93-d5ee2935d2cb.mock.pstmn.io/get-json" -r json"', function (code) {
+        exec('node ./bin/newman.js request -X GET "https://5a178277-8664-409c-8e93-d5ee2935d2cb.mock.pstmn.io/get-json" -r progress', function (code) {
             expect(code, 'should have exit code of 0').to.equal(0);
             done();
         });
